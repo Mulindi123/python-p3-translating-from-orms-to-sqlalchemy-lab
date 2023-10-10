408f7a3ee55c1ce517be679c0e3fbe21c07d8779
Mulindi123 <medrine.mulindi@gmail.com>
2023-10-10 10:21:56 +0300

    Completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer, create_engine)
+from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
