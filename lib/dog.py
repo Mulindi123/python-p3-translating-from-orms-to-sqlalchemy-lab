66a252820f5cc63d51f434ed2e7c50c0ba213ee5
Mulindi123 <medrine.mulindi@gmail.com>
2023-10-10 10:18:23 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
-from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Dog, Base as base
 
+
+engine = create_engine("sqlite:///dogs.db")
 def create_table(base):
-    pass
+   
+    base.metadata.creat_all(engine)
 
 def save(session, dog):
-    pass
+   session.add(dog)
+   session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+if __name__ == "__main__":
+    create_table(base)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
